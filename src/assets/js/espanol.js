2c1ddda740c01079b5620fa9ebd55438cf379bbd
jacobzacarias <101657772+jacobzacarias@users.noreply.github.com>
2025-08-05 12:27:44 -0500

    Debugging visual styles and updated formatting for new updates

--- src/assets/js/espanol.js
+++ src/assets/js/espanol.js
@@ -1,6 +1,9 @@
 /* Body and Toggle */
 var body = document.querySelector("body");
-const espanolToggle = document.querySelector("#espanol-mode-toggle");
+const espanolToggle = document.getElementById("espanol-mode-toggle");
+
+// var body = document.querySelector("body");
+// const espanolToggle = document.querySelector("#espanol-mode-toggle");
 
 // Get Status of Language
 let espanol = localStorage.getItem("espanol");
@@ -8,25 +11,25 @@ let espanol = localStorage.getItem("espanol");
 // Enable Language Mode
 const enableEspanol = () => {
   body.classList.add("espanol-mode");
-  localStorage.setItem("espanolMode", "enabled")
+  localStorage.setItem("espanol", "enabled")
 }
 
 // Disable Language
 const disableEspanol = () => {
   body.classList.remove("espanol-mode");
-  localStorage.setItem("espanolMode", null)
+  localStorage.setItem("espanol", null)
 }
 
 // Check the state of language mode in local storage
-if (espanolMode == "enabled") {
+if (espanol == "enabled") {
   enableEspanol();
 }
 
 // add event listener to the language mode button toggle
 espanolToggle.addEventListener('click', () => {
   // on click, check localstorage for the language mode value
-  espanolMode = localStorage.getItem("espanolMode");
-  if (espanolMode !== "enabled") {
+  espanol = localStorage.getItem("espanol");
+  if (espanol !== "enabled") {
     // if lanuage mode is not enabled, run this function to set it to enabled
     enableEspanol();
   } else {
